d9418a4502af9098b0c1a61e89acd07f95eabf34
nikhil mishra <120741548+nikhilmishra712@users.noreply.github.com>
2025-04-03 19:39:09 +0000

    change cont conf

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -1,1 +1,8 @@
 console.log(" nikjil");
+const accountId = 12345
+let accountEmail = "niks@google.com"
+var accountpassword ="5657" 
+accountCity = "bhopal"
+acccountloc = " narela"
+accountdatee = " 4562"
+console.table([ accountEmail,accountId,accountpassword,acccountloc,accountCity])
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,20 @@
+console.log("ram");
+let Age = 18
+
+
+
+
+
+
+// number = 2 to the power 53
+// bigInt for bigger no 
+//bollen= true/false
+// string = "_______"
+// null = stand alone value
+//undefined = 
+
+
+
+console.log(Age);
+
+console.log(typeof " age");
